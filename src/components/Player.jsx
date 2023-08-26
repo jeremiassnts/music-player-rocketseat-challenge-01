6c35de9e5f5cb1942dc8d4b4e437ec9c0d2d08e6
Jeremias Santos <jeremias.santos@newcheck.com.br>
2023-08-26 18:51:20 -0300

    feat: timeline working

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,8 +5,45 @@ import { Info } from './Info'
 import { FaPlay, FaPause, FaForward, FaBackward } from 'react-icons/fa'
 import { Button } from './Button'
 import { Timeline } from './Timeline'
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Player() {
+    var duration = "00:10"
+    var durationInSeconds = 10
+
+    const [now, setNow] = useState("00:00")
+    const [nowInSeconds, setNowInSeconds] = useState(0)
+    const [left, setLeft] = useState(duration)
+    const [total] = useState(duration)
+    const [totalInSeconds] = useState(durationInSeconds)
+    const [percentage, setPercentage] = useState(0)
+
+    function musicTimeout() {
+        setNowInSeconds(prev => {
+            if (prev >= totalInSeconds) {
+                return prev
+            }
+            else {
+                var day = dayjs().format('YYYY-MM-DD')
+                var timeNow = dayjs(`${day} 00:00:00`).add(prev + 1, 'second')
+                var timeLeft = dayjs(`${day} 00:${total}`).subtract(prev + 1, 'second')
+
+                setNow(timeNow.format('mm:ss'))
+                setLeft(timeLeft.format('mm:ss'))
+                setPercentage(Math.ceil((prev + 1) / totalInSeconds * 100))
+                return prev + 1
+            }
+        })
+    }
+
+    useEffect(() => {
+        const timeout = setInterval(musicTimeout, 1000);
+        return () => {
+            clearInterval(timeout)
+        }
+    }, []);
+
     return (
         <div className={style.container}>
             <div className={style.coverinfo}>
@@ -18,7 +55,7 @@ export function Player() {
                 <Button Icon={FaPlay} color="#E1E1E6" size="1.2em" />
                 <Button Icon={FaForward} color="#E1E1E6" size="1.2em" />
             </div>
-            <Timeline className={style.timeline} now="03:20" left="00:12" />
+            <Timeline className={style.timeline} now={now} left={left} percentage={percentage} />
         </div>
     )
 }
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,15 +1,15 @@
 import style from './Timeline.module.css'
 
-export function Timeline({ now, left }) {
+export function Timeline({ now, left, percentage }) {
     return (
         <div className={style.container}>
             <div className={style.line}>
-                <div></div>
+                <div style={{ width: `${percentage}%` }}></div>
             </div>
             <div className={style.times}>
                 <span>{now}</span>
                 <span>{left}</span>
             </div>
-        </div>
+        </div >
     )
 }
